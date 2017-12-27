ffcd35cb61f7685c204f393f19b2575509abd54c
andrew3007 <andrew3007@71bdbfee-cdf7-11e7-a777-b37b4a7f8ca7>
2017-12-27 19:12:32 +0000

    Update runWebServer.bat
    git-svn-id: https://english-dictionary.svn.beanstalkapp.com/english-dictionary/trunk@60 71bdbfee-cdf7-11e7-a777-b37b4a7f8ca7

--- build/runWebServer.bat
+++ build/runWebServer.bat
@@ -9,7 +9,8 @@ SET JAVA_HOME=%JAVA8_HOME%
 SET PATH=%JAVA8_HOME%\bin;%PATH%
 
 SET mainOptions=-Djava.home="%JAVA8_HOME%" -Dfile.encoding=UTF-8 -server ^
-                -Xms1024m -Xmx3024m -XX:ReservedCodeCacheSize=512m -XX:+UseConcMarkSweepGC -XX:SoftRefLRUPolicyMSPerMB=50 ^
-                -ea -Dsun.io.useCanonCaches=false -Djava.net.preferIPv4Stack=true
+                -Xms2024m -Xmx4024m -XX:ReservedCodeCacheSize=512m -XX:+UseConcMarkSweepGC -XX:SoftRefLRUPolicyMSPerMB=50 ^
+				-Dio.netty.maxDirectMemory:3758096384 ^
+                -ea -Dsun.io.useCanonCaches=false -Djava.net.preferIPv4Stack=false
 
 cmd /k %JAVA8_HOME%/bin/java %remoteDebugOptions% %mainOptions% -jar ./englishDictionary.jar
