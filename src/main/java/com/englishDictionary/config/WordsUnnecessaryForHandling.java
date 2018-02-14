2a379c492adcd54db3054e9d3df397abd74b2fc5
Andrei Kabanov <andrew3007@rambler.ru>
2018-02-14 22:02:13 +0300

    List of unnecessary words were extended

--- src/main/java/com/englishDictionary/config/WordsUnnecessaryForHandling.java
+++ src/main/java/com/englishDictionary/config/WordsUnnecessaryForHandling.java
@@ -182,9 +182,7 @@ public class WordsUnnecessaryForHandling {
             "with",
             "without",
             "within",
-            "according",
-            "beyond",
-            "whereas"
+            "according"
     ).collect(Collectors.toSet());
 
     public static boolean isNecessaryWord(String word) {
